b20a30b5213bd22396cfb3629f691a5a0cc44c24
ArtemPopov18 <Arshawin1991gmail.com>
2023-01-17 19:17:14 +0700

    Все работает

---
+++ m8_quiz_animation/app/src/main/java/com/example/skillbox_hw_quiz/MainActivity.kt
@@ -0,0 +1,21 @@
+package com.example.skillbox_hw_quiz
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    //    override fun onCreate(savedInstanceState: Bundle?) {
+//        super.onCreate(savedInstanceState)
+//        setContentView(R.layout.main_activity)
+//        if (savedInstanceState == null) {
+//            supportFragmentManager.beginTransaction()
+//                .replace(R.id.container, MainFragment.newInstance())
+//                .commitNow()
+//        }
+//    }
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.main_activity)
+    }
+}
