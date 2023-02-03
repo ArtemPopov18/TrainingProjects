cde38ac814f92c9382b9b0f7e962e59df8b30f6f
ArtemPopov18 <Arshawin1991gmail.com>
2023-02-03 13:08:30 +0700

    Заработало!

--- m17_recyclerview/app/src/main/java/com/popov/recyclerview/presation/ImageMarsFragment.kt
+++ m17_recyclerview/app/src/main/java/com/popov/recyclerview/presation/ImageMarsFragment.kt
@@ -1,5 +1,6 @@
 package com.popov.recyclerview.presation
 
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.util.Log
 import android.view.LayoutInflater
@@ -7,6 +8,10 @@ import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
 import com.bumptech.glide.Glide
+import com.bumptech.glide.load.DataSource
+import com.bumptech.glide.load.engine.GlideException
+import com.bumptech.glide.request.RequestListener
+import com.bumptech.glide.request.target.Target
 import com.popov.recyclerview.databinding.FragmentImageMarsBinding
 
 
@@ -33,6 +38,28 @@ class ImageMarsFragment : Fragment() {
         Log.d("AAAA", url.toString())
         Glide.with(requireActivity())
             .load(url)
+//            .listener(object : RequestListener<Drawable?>{
+//                override fun onLoadFailed(
+//                    e: GlideException?,
+//                    model: Any?,
+//                    target: Target<Drawable?>?,
+//                    isFirstResource: Boolean
+//                ): Boolean {
+//                    Log.e("TAG", "Error loading image", e)
+//                    return false // important to return false so the error placeholder can be placed
+//                }
+//
+//                override fun onResourceReady(
+//                    resource: Drawable?,
+//                    model: Any?,
+//                    target: Target<Drawable?>?,
+//                    dataSource: DataSource?,
+//                    isFirstResource: Boolean
+//                ): Boolean {
+//                    return false
+//                }
+//
+//            })
             .into(binding.imageMarsView)
     }
 
